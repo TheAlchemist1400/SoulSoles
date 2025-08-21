ead0107e2f7f4de8c69b94af60f38da4a37c0ada
TheAlchemist1400 <armando.moabi@gmail.com>
2025-08-22 00:22:04 +0200

    Prepare branch for Postgree & staticfiles

--- soulsoles_project/settings.py
+++ soulsoles_project/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'store', 'static')  # adjust if your app is named differently
+    os.path.join(BASE_DIR, 'store', 'static') 
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
